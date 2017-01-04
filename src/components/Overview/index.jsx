5d8c89abc6ba8e017568fd752ebab204e5fe22be
visya <m.verbenko@gmail.com>
2017-01-04 17:05:32 +0100

    feat(overview): Add immutable stats prop

--- src/components/Overview/index.jsx
+++ src/components/Overview/index.jsx
@@ -1,28 +1,34 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import { List, fromJS } from 'immutable';
 
 import Paper from 'material-ui/Paper';
 import Stat from 'components/Stat';
 
 import styles from './styles.scss';
 
-const stats = [
+const mockStats = fromJS([
   { label: 'open', value: 15 },
   { label: 'ready to merge', value: 5 },
   { label: 'waiting for review', value: 10 },
   { label: 'lines per pr', value: 100 },
   { label: 'files per pr', value: 5.5 },
-];
+]);
 
-const Overview = () => (
+const Overview = ({
+  stats = mockStats,
+}) => (
   <Paper className={ styles.overview }>
     {
       stats.map(stat => (<Stat
-        value={ stat.value }
-        label={ stat.label }
-        key={ stat.label }
+        value={ stat.get('value') }
+        label={ stat.get('label') }
+        key={ stat.get('label') }
       />))
     }
   </Paper>
 );
+Overview.propTypes = {
+  stats: PropTypes.instanceOf(List),
+};
 
 export default Overview;
